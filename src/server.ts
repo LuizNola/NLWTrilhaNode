62fc34279ff5d910f112926a3e7302540d38f489
Luiz Nola <luizn647@gmail.com>
2021-02-23 20:14:36 -0300

    configurando o DB

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
+import 'reflect-metadata'
 import express from 'express';
+import './database';
 
 const app = express();
 
 let PORT = 3333 
 
-app.get("/users")
+app.get("/users", (req, res) => {
 
-app.listen(PORT, () => {console.log(`Ru
+    return res.send('Hello Word!')
+})
+
+app.listen(PORT, () => {console.log(`Running in ${PORT}!`)})
