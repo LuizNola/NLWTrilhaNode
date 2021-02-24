bdb11fec6ae6ddca5de8a6deb702d3e35b40e081
Luiz Nola <luizn647@gmail.com>
2021-02-23 21:35:10 -0300

    adicionando usuario

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,13 @@
 import 'reflect-metadata'
 import express from 'express';
 import './database';
+import { router } from './routes';
 
 const app = express();
 
 let PORT = 3333 
 
-app.get("/users", (req, res) => {
-
-    return res.send('Hello Word!')
-})
+app.use(express.json());
+app.use(router);
 
 app.listen(PORT, () => {console.log(`Running in ${PORT}!`)})
